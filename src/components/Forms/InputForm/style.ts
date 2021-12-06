1434f2194bc8e90b92a0ab07e003bef7057b7929
Rafael Anzini Ximenes <anzinirafael50@gmail.com>
2021-12-06 12:18:37 -0300

    update 06/12/21

---
+++ src/components/Forms/InputForm/style.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components/native';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+export const Container = styled.View`
+    width: 100%;
+`
--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,12 +1,12 @@
 import React, {useState} from 'react';
 import {Modal} from 'react-native';
-import { useForm, Controller } from "react-hook-form";
-
+import { useForm} from "react-hook-form";
 import { Button } from '../../components/Forms/Button';
 import { Input } from '../../components/Forms/Input';
 import { SelectCategory } from '../../components/Forms/SelectCategory';
 import { Transactions } from '../../components/Forms/Trasanctions';
 import { CategorySelect } from '../CategorySelect';
+import { InputForm } from '../../components/Forms/InputForm';
 import {
     Container,
     Header,
@@ -15,13 +15,22 @@ import {
     Fields,
     TransactionsButtons} from './style';
 
-export function Register(){
+interface FormData{
+    name: string,
+    amount: string
+}
+
+export function Register({name, amount} : FormData){
     const [category, setCategory] = useState({
         key: 'category',
         name: 'Categoria',
     });
     const [selectionButton, setSelectionButton] = useState('');
     const [openModal, setOpenModal] = useState(false);
+    const {
+        control,
+        handleSubmit
+    } = useForm();
 
     function handleSelectionButton(type: 'up' | 'down'){
         setSelectionButton(type)
@@ -35,6 +44,17 @@ export function Register(){
         setOpenModal(false)
     }
 
+    function handleRegister(form : FormData){
+        const data ={
+            name: form.name,
+            amount: form.amount,
+            selectionButton,
+            category: category.key
+        }
+        console.log(data);
+    }
+
+
     return(
         <Container>
             <Header>
@@ -44,15 +64,15 @@ export function Register(){
             </Header>
             <Form>
                 <Fields>
-                    <Input placeholder="Nome" style={{marginBottom: 8}}/>
-                    <Input placeholder="Preço"/>  
+                    <InputForm name="name" control={control} placeholder="Nome" style={{marginBottom: 8}}/>
+                    <InputForm name="amount" control={control} placeholder="Preço"/>  
                     <TransactionsButtons>
                         <Transactions title="Income" type="up" isActive={selectionButton  === 'up'} onPress={() => handleSelectionButton('up')}/>
                         <Transactions title="Income" type="down" isActive={selectionButton  === 'down'} onPress={() => handleSelectionButton('down')}/>
                     </TransactionsButtons>
                     <SelectCategory title={category.name} onPress={handleCategoryOpenModal} />
                 </Fields>
-                <Button title="Enviar"/>
+                <Button title="Enviar" onPress={handleSubmit(handleRegister)}/>
             </Form>
             <Modal visible={openModal}>
                 <CategorySelect         
