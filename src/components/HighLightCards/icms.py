fa07147b5c885b30bd4d19ec437d4ef77fbd98f5
Rafael Anzini Ximenes <anzinirafael50@gmail.com>
2022-01-28 08:04:17 -0300

    update

--- src/components/HighLightCards/icms.py
+++ src/components/HighLightCards/icms.py
@@ -3,7 +3,7 @@ valorDoICMS = 0;
 origem = "SC";
 destino = str(input("Insira o destino do produto: "));
 
-if destino == 'MG' or destino == 'PR' or destino == 'RS' or destino == "RJ":
+if destino == 'MG' or destino == 'PB' or destino == 'RS' or destino == "RJ" or destino == "SP":
     valorDoICMS = 0.15;
     calcICMS =  valorProduto - ((1 - valorDoICMS) * valorProduto);
 else:
